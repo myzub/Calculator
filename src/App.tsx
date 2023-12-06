dbd897bf9b08b9ff5e4ff39adf5c4e7699115722
myzub <zub.m345@gmail.com>
2023-12-06 13:29:33 +0100

    4 - calculation logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// noinspection TypeScriptValidateTypes
+
 import { useState } from "react";
 import Button from "./components/Button";
 import Sun from "./img/Sun";
@@ -5,43 +7,147 @@ import Burger from "./img/Burger";
 import Cross from "./img/Cross";
 import Square from "./img/Square";
 import Dash from "./img/Dash";
-import Backspace from "./img/Backspace";
+
+const btnValues = [
+  ["AC", "<", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  ["+-", 0, ",", "="],
+];
+
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
 function App() {
-  const defaultResult = 0;
   const defaultResultFontSize = 88;
-  const [result, setResult] = useState(defaultResult);
   const [resultFontSize, setResultFontSize] = useState(defaultResultFontSize);
+  const [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
 
-  function enterDigit(value) {
-    const newResult = result * 10 + value;
-    const newLength = newResult.toString().length;
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
 
-    if (newLength > 13) {
-      return;
-    } else if (newLength > 6) {
-      setResultFontSize((prevState) => prevState - 8);
+    if (removeSpaces(calc.num).length < 13) {
+      if (removeSpaces(calc.num).length > 5) {
+        console.log(resultFontSize);
+        if (resultFontSize < 68) {
+          return;
+        } else setResultFontSize((prevState) => prevState - 9);
+      } else setResultFontSize(defaultResultFontSize);
+
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      });
     }
-    setResult(newResult);
-  }
+  };
 
-  function deleteDigit() {
-    const newResult = result / 10;
-    const newLength = newResult.toString().length;
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
 
-    setResult(Math.floor(newResult));
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
 
-    if (newLength > 7) {
-      setResultFontSize((prevState) => prevState + 8);
-    } else {
-      setResultFontSize(defaultResultFontSize);
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+  const equalClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Can't divide with 0"
+            : toLocaleString(
+                math(
+                  Number(removeSpaces(calc.res)),
+                  Number(removeSpaces(calc.num)),
+                  calc.sign
+                )
+              ),
+        sign: "",
+        num: 0,
+      });
     }
-  }
+  };
 
-  function clearResult() {
-    setResult(defaultResult);
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+      sign: "",
+    });
+  };
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+
+  const backspaceClickHandler = () => {
+    if (removeSpaces(calc.num).length > 6)
+      setResultFontSize((prevState) => prevState + 9);
+    else setResultFontSize(defaultResultFontSize);
+
+    setCalc((prev) => ({
+      ...prev,
+      num: toLocaleString(
+        Number(removeSpaces(prev.num.toString().slice(0, -1)))
+      ),
+    }));
+  };
+
+  const resetClickHandler = () => {
     setResultFontSize(defaultResultFontSize);
-  }
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
 
   return (
     <>
@@ -65,7 +171,7 @@ function App() {
         </div>
         <div className="mt-[80px] px-5">
           <div className="text-zinc-600 text-2xl font-light font-['Poppins'] flex justify-end">
-            100 + 49 + 7 + 451 + 30 {/*TODO*/}
+            100 + 49 / 7 - 451 * 30
           </div>
           <div
             id="resultContainer"
@@ -75,85 +181,39 @@ function App() {
               id="resultSpan"
               style={{ transition: "font-size 0.3s", fontSize: resultFontSize }}
             >
-              {result}
+              {calc.num ? calc.num : calc.res}
             </span>
           </div>
         </div>
+
         <div className="grid grid-cols-4 grid-flow-row gap-x-3 gap-y-3.5 m-5 font-normal text-center text-[34px] font-['Poppins']">
-          <Button
-            value="AC"
-            dark={true}
-            addClass="text-center text-[34px] font-medium"
-            onClick={() => clearResult()}
-          />
-          <Button
-            dark={true}
-            value={<Backspace />}
-            onClick={() => deleteDigit()}
-          />
-          <Button
-            value="%"
-            dark={true}
-            addClass="text-5xl"
-            onClick={setResult}
-          />
-          <Button
-            value="÷"
-            dark={true}
-            addClass="text-5xl"
-            // onClick={setResult}
-          />
-
-          <Button value={7} onClick={() => enterDigit(7)} />
-          <Button value={8} onClick={() => enterDigit(8)} />
-          <Button value={9} onClick={() => enterDigit(9)} />
-          <Button
-            value="x"
-            dark={true}
-            addClass="text-5xl rotate-45"
-            // onClick={setResult}
-          />
-
-          <Button value={4} onClick={() => enterDigit(4)} />
-          <Button value={5} onClick={() => enterDigit(5)} />
-          <Button value={6} onClick={() => enterDigit(6)} />
-          <Button
-            value="-"
-            dark={true}
-            addClass="text-5xl"
-            // onClick={setResult}
-          />
-
-          <Button value={1} onClick={() => enterDigit(1)} />
-          <Button value={2} onClick={() => enterDigit(2)} />
-          <Button value={3} onClick={() => enterDigit(3)} />
-          <Button
-            value="+"
-            dark={true}
-            // onClick={setResult}
-          />
-
-          <Button
-            value={
-              <img
-                src="src/img/plus-minus.png"
-                alt="&#177;"
-                className=" w-[24px] m-auto"
+          {btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                dark={!(typeof btn === "number" || btn === "+-" || btn === ",")}
+                addClass={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={
+                  btn === "AC"
+                    ? resetClickHandler
+                    : btn === "<"
+                    ? backspaceClickHandler
+                    : btn === "+-"
+                    ? invertClickHandler
+                    : btn === "%"
+                    ? percentClickHandler
+                    : btn === "="
+                    ? equalClickHandler
+                    : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                    ? signClickHandler
+                    : btn === "."
+                    ? commaClickHandler
+                    : numClickHandler
+                }
               />
-            }
-          />
-          <Button value={0} onClick={() => enterDigit(0)} />
-          <Button
-            value="."
-            addClass="text-[44px]"
-            // onClick={setResult}
-          />
-          <Button
-            value="="
-            dark={true}
-            addClass="text-[44px]"
-              // onClick={setResult}
-          />
+            );
+          })}
         </div>
       </div>
     </>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,8 @@ const Button = ({ value, dark, addClass, onClick }) => {
             ? "bg-func-button-color text-white"
             : "bg-digits-button-color text-black"
         } ${cls} w-[88px] h-[88px] rounded-[352px] bg-digits-button-color`}
+        type="button"
+        value={value}
         onClick={onClick}
       >
         {value}
